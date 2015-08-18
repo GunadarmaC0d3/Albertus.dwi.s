d0a23a7715af299ef9fe91011e0daa08645dbe92
dizolette <albertusdwie@gmail.com>
2015-08-18 14:54:51 +0700

    Tipe Data
    tipe data yang sering digunakan pada Java

---
+++ Java/TipeData.java
@@ -0,0 +1,35 @@
+public class TipeData {
+
+	public static void main(String[] args){
+		//deklarasi tipe data -> tipe data identifier/variabel
+		//tipe data Integer (default) (tipe data yang berupa angka bulat)
+		int x = 10;
+		
+		//tipe data double (default, 64-bit) (tipe data untuk menyimpan data berupa desimal)
+		double c = 2.123456789123456789;
+		
+		//tipe data float (32-bit) (tipe data untuk menyimpan data berupa desimal dengan menambahkan "f"/"F")
+		//ketelitian lebih rendah daripada double
+		float b = 2.123456789123456789f;
+		
+		//tipe data String (tipe data untuk menyimpan teks, yang harus berada dalam tanda " " )
+		String pet = "dog";
+		
+		//Tipe data char (tipe data ini hanya dapat menyimpan 1 karakter ex: a )
+		char ex = 'a' ;
+		
+		//tipe data boolean (tipe data yang nilainya hanya true/false)
+		boolean y = true;
+		
+		//print output
+		System.out.println(x);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(pet);
+		System.out.println(ex);
+		System.out.println(y);
+	}
+
+
+
+}
