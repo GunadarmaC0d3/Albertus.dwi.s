cb8c9735043437950cf92be359c3edfabc9cf4d8
Albertus <albertusdwie@gmail.com>
2015-08-18 14:23:58 +0700

    Update HelloWorld.java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -15,9 +15,9 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // System.out.println("text"/variabel);
+        // menampilkan output
         System.out.println("Hello World in Java :)");
-        //latihan print
     }
     
 }
