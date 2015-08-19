99aa3267d11408af79eb6a30503925a5098ce9d0
dizolette <albertusdwie@gmail.com>
2015-08-19 10:53:54 +0700

    perbedaan print dengan println

---
+++ Java/Print.java
@@ -0,0 +1,20 @@
+public class Print{
+	
+	public static void main(String[] args){
+		/* cara menampilkan text atau nilai dari variabel dapat menggunakan:
+		System.out.print(); atau System.out.println();
+		*/
+		
+		// System.out.print
+		System.out.print("Hello ");
+		System.out.print("world!");
+		System.out.println("");
+		
+		// System.out.println
+		System.out.println("Hasil println: ");
+		System.out.println("Belajar");
+		System.out.println("Java");
+	}
+
+
+}
